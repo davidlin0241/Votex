98fb62067504ba69321f934aa355f87e14891f91
davidlin0241 <davidlin0241@gmail.com>
2019-09-11 09:19:17 -0700

    Update views.py

--- draw/views.py
+++ draw/views.py
@@ -8,7 +8,7 @@ def index(request):
 # def room(request, room_name):
 #     return render(request, 'draw/room.html', {
 #         'room_name_json': mark_safe(json.dumps(room_name))
-#     })
+#     }) 
  
 def voteLarge(request):
    return render(request, "draw/voteLarge.html")
